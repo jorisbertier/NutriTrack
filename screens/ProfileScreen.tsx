07b6ebfe56e6b20f5f7fb5415dd5452e4f99ec27
Joris Bertier <joris.bertier@gmail.com>
2024-11-26 08:35:08 -0600

    add avatar

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -67,7 +67,7 @@ const ProfileScreen = () => {
   return (
     <ScrollView contentContainerStyle={[styles.container, {backgroundColor: colors.whiteMode}]}>
       <View style={styles.profileHeader}>
-      {isLoading ? <Image source={require('@/assets/images/profil/avatar.png')} style={styles.profileImage} />  : <Skeleton colorMode={colorMode} height={120} width={120} radius={'round'}/> }
+      {isLoading ? <Image source={require('@/assets/images/avatar/avatar.png')} style={styles.profileImage} />  : <Skeleton colorMode={colorMode} height={120} width={120} radius={'round'}/> }
       {/* {isLoading ? <Image source={{ uri: `data:image/jpeg;base64,${userData[0]?.profilPicture}` }} style={styles.profileImage} />  : <Skeleton colorMode={colorMode} height={120} width={120} radius={'round'}/> } */}
         {isLoading ? <Text style={[styles.name, { color: colors.black}]}>{userData[0]?.firstName} {userData[0]?.name}</Text> : <View style={{marginTop: 5}}><Skeleton colorMode={colorMode} width={150} /></View> }
         {isLoading ? <Text style={[styles.email, { color: colors.black}]}>{userData[0]?.email}</Text> : <View style={{marginTop: 5}}><Skeleton colorMode={colorMode} width={250} /></View> }
