08bf3a32d2b1e3a43dbd5769bd1c4afafa3df707
Joris Bertier <joris.bertier@gmail.com>
2025-01-14 10:37:34 -0500

    change text

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -110,7 +110,7 @@ const ProfileScreen = () => {
       {!isLoading ? <Image source={avatar} style={styles.profileImage} />  : null }
       </Skeleton>
       {/* {isLoading ? <Image source={{ uri: `data:image/jpeg;base64,${userData[0]?.profilPicture}` }} style={styles.profileImage} />  : <Skeleton colorMode={colorMode} height={120} width={120} radius={'round'}/> } */}
-        {!isLoading ? <Text style={[styles.name, { color: colors.black}]}>{userData[0]?.firstName} {userData[0]?.name}</Text> : <View style={{marginTop: 5}}><Skeleton colorMode={colorMode} width={150} /></View> }
+        {!isLoading ? <Text style={[styles.name, { color: colors.black}]}>{userData[0]?.name} {userData[0]?.firstName}</Text> : <View style={{marginTop: 5}}><Skeleton colorMode={colorMode} width={150} /></View> }
         {!isLoading ? <Text style={[styles.email, { color: colors.black}]}>{userData[0]?.email}</Text> : <View style={{marginTop: 5}}><Skeleton colorMode={colorMode} width={250} /></View> }
       </View>
       <View style={[styles.section, {backgroundColor: colors.white}]}>
